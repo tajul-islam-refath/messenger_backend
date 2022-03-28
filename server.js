a50e685cfb69f853d6a24c960cbd6708a7dc51bf
Tajul Islam Refath <tajul16-447@diu.edu.bd>
2022-03-28 08:57:29 +0600

    Add socket.io and setup it && create a room for user

--- server.js
+++ server.js
@@ -11,17 +11,41 @@ if (process.env.NODE_ENV != "PRODUCTION") {
 const dbName = process.env.DB_NAME;
 const dbPass = process.env.DB_PASS;
 const url = `mongodb+srv://${dbName}:${dbPass}@cluster0.ltldm.mongodb.net/messenger?retryWrites=true&w=majority`;
+let server;
 mongoose
   .connect(url, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   })
   .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log(`Server Started on PORT ${process.env.PORT}`);
-      console.log("Database Connect Success");
-    });
+    console.log("Database Connect Success");
   })
   .catch((e) => {
     return console.log(e);
   });
+
+server = app.listen(process.env.PORT, () => {
+  console.log(`Server Started on PORT ${process.env.PORT}`);
+});
+
+// socket.io setup
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+// connect socket io
+io.on("connection", (socket) => {
+  console.log("socket.io connection is done");
+
+  // received user from client
+  socket.on("setup", (userData) => {
+    // console.log(userData);
+    // join user in room
+    socket.join(userData._id);
+    // send response back to client
+    socket.emit("response");
+  });
+});
